efb199228386c4becef4f7df3bf8c4873a262411
yxtonic <yxtonic@gmail.com>
2014-09-12 16:36:52 -0700

    changed to portrait view

---
+++ SpellRunner.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  SpellRunner
+//
+//  Created by Toni Chen on 9/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
---
+++ SpellRunner.spritebuilder/Gameplay.m
@@ -0,0 +1,23 @@
+//
+//  Gameplay.m
+//  SpellRunner
+//
+//  Created by Toni Chen on 9/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay
+
+    CCTextField *_spellingbox;
+//
+//-(void) onEnter
+//{
+//    [super onEnter];
+//    _spellingbox.positionType=CCPositionTypeNormalized;
+//    _spellingbox.position= ccp(.5f,.5f);
+//
+//}
+
+@end
